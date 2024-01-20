f2203c050e698938e116a70f7540ec13e9116ee6
pradeephs17 <pradeephs17@gmail.com>
2024-01-20 15:31:13 +0530

    com

---
+++ PROJECT1/emp.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[emp]
+(
+  emp_id INT NOT NULL PRIMARY KEY,
+  empname VARCHAR(20),
+  salary int
+)
+
+select * from emp;
+insert into emp values(1,'ram',1000)
---
+++ Sample/sa/emp
@@ -0,0 +1,7 @@
+create table emp
+(
+    emp_id int,
+    emp_name varchar(20),
+    salary int
+
+)
